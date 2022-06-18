e9d0289c8542e1c3874a17ca49eb348d3095315b
Tbonator <thabo2g5@yahoo.ca>
2022-06-18 13:04:36 +0200

    first commit

---
+++ projects/urls.py
@@ -0,0 +1,11 @@
+from django.urls import  path
+from . import views
+
+
+
+#import views into this file 
+
+urlpatterns = [
+    path('',views.projects,name="projects"),
+    path('project/<str:pk>',views.project,name="project"),
+]
