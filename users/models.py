68019767db2394d15c515cb6cc71cbbb0ed8395e
Tbonator <thabo2g5@yahoo.ca>
2022-07-06 22:27:11 +0200

    adding signals

--- users/models.py
+++ users/models.py
@@ -1,10 +1,7 @@
-from pyexpat import model
 import uuid
 from django.db import models
+
 from django.contrib.auth.models import User
-
-
-# Create your models here.
 
 
 class Profile(models.Model):
@@ -17,8 +14,10 @@ class Profile(models.Model):
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(
-        blank=True, null=True, upload_to="profile-pics/", default ="../static/images/profile-pics/user-default.png")
+        blank=True, null=True, upload_to="profile-pics/", default="../static/images/profile-pics/user-default.png")
     social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_stackoverflow = models.CharField(
+        max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
     social_linkedin = models.CharField(max_length=200, blank=True, null=True)
     social_youtube = models.CharField(max_length=200, blank=True, null=True)
@@ -26,16 +25,21 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
     def __str__(self):
-       return str(self.user.username)
-   
-   
+        # return str(self.user.username)
+        return str(self.username)
+
+
 class Skill(models.Model):
-    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,blank=True, null=True)
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
     def __str__(self):
-       return str(self.name)
+        return str(self.name)
+
--- users/views.py
+++ users/views.py
@@ -9,5 +9,10 @@ def profiles (request):
 
 def userProfile(request,pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile': profile }
-    return render (request, 'users/user-profile.html',context)
+    # exclude  topskill with empty description
+    topSkills = profile.skill_set.exclude(description__exact="")
+    # exclude  otherskill with empty description
+    otherSkills = profile.skill_set.filter(description = "")
+    
+    context = {'profile': profile, 'topSkills': topSkills,'otherSkills': otherSkills}
+    return render(request, 'users/user-profile.html', context)
